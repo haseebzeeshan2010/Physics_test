e6b7f5205464d9a0ec3c47fc9f42a5bd3403cd1a
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-05-16 17:06:28 +0100

    Add drift detection and gradient line indicator

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -1,9 +1,43 @@
 extends RigidBody2D
 var impulse_strength = 38
-
+var drag_change = 0
+var new_skid = 0
+var remove_skid = false
 func _physics_process(delta):
 	#look_at(get_global_mouse_position())
+	#SKID CODE
+	var speed = linear_velocity.length()
+	if speed > 500 and speed < 850:
+		#print("DRAG")
+		#print(speed)
+		if new_skid == 0:
+			remove_skid = false
+			skid()
+	else:
+		remove_skid = true
+		new_skid = %Skid_mark.get_point_count()
+	if remove_skid == true:
+		%Skid_mark.remove_point(0)
+
 	
+	#TURNING CODE
+	if Input.is_action_pressed("mouse_acceleration"):
+		impulse_strength = 38
+		var m = get_global_mouse_position()
+		var aim_speed = deg_to_rad(1)
+		if get_angle_to(m) < 0.02 and get_angle_to(m) > -0.2:
+			constant_torque = 0
+		else:
+			if get_angle_to(m) > 0:
+				constant_torque = 30000
+			else:
+				constant_torque = -30000
+	
+	else:
+		impulse_strength = 0
+		constant_torque = 0
+	
+	#THRUST CODE
 	var angle = rotation
 	
 	if position.distance_to(get_global_mouse_position()) > 5:
@@ -11,21 +45,12 @@ func _physics_process(delta):
 	#print(get_angle_to(get_global_mouse_position()))
 	
 	
+
+func skid():
+	%Skid_mark.add_point(global_position)
+	while %Skid_mark.get_point_count() > 50:
+		%Skid_mark.remove_point(0)
 		
-	
-	#STARBLAST LIKE ROTATION
-	var m = get_global_mouse_position()
-	var aim_speed = deg_to_rad(1)
-	if get_angle_to(m) < 0.02 and get_angle_to(m) > -0.2:
-		constant_torque = 0
-		#constant_torque = -constant_torque
-	else:
-		if get_angle_to(m) > 0:
-			constant_torque = 60000
-			#rotation = lerp(rotation,rotation+aim_speed,delta*300)
-			#angular_velocity = 10
-		else:
-			constant_torque = -60000
-			#rotation = lerp(rotation,rotation-aim_speed,delta*300)
-			#angular_velocity = -10
+	drag_change = %Skid_mark.get_point_count()
+
 
